a713bbbc197a8cfb46023458b34ac6c2f6d6ea24
riptakagi <smatchjp@gmail.com>
2021-04-04 01:38:53 +0900

    不要な処理を削除

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import BotClient from './struct/BotClient'
 
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ', listener.address());
-});
-
 const client = new BotClient();
 
 client.start(process.env.TOKEN);
